b1b0871ff0e9d76e44589db2fcf15d18ad1f6553
mcomen <mcomentr@yahoo.com>
2018-01-13 21:05:15 -0500

    About Page is built.

---
+++ Beewz/AboutViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AboutViewController.swift
+//  Beewz
+//
+//  Created by MUHAMMED COMEN on 1/13/18.
+//  Copyright © 2018 MUHAMMED COMEN. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var aboutText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
